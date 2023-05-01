095b25cfe816fc3c3693b54da41b0d7ebc16fd53
wijdane <wa20010702@gmail.com>
2023-05-01 09:27:49 -0700

    cmt

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,24 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t lemona = 1;
-if(h == NULL)
-return (0);
-while (h->next != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-lemona++;
-}
-printf("%d\n", h->n);
-return (lemona);
+	size_t node_count = 1;
+
+	/* return 0 as no of nodes when h is null*/
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+
+		/*go to the next node*/
+		h = h->next;
+		/*count no of nodes*/
+		node_count++;
+	}
+
+	/*print last node*/
+	printf("%d\n", h->n);
+
+	return (node_count);
 }
